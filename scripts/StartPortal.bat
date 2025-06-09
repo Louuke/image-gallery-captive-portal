9fa398ac174114f9e10d2651aaaa09f881ea577b
Louke <git@jannsen.org>
2025-06-09 12:49:05 +0200

    Add StartPortal.bat to launch Docker Compose and start tethering

--- scripts/StartPortal.bat
+++ scripts/StartPortal.bat
@@ -1,6 +1,6 @@
 @echo off
 
-powershell -ExecutionPolicy -ByPass "$profile = [Windows.Networking.Connectivity.NetworkInformation,Windows.Networking.Connectivity,ContentType=WindowsRuntime]::GetConnectionProfiles() | where {$_.profilename -eq 'loopback'}; $tether = [Windows.Networking.NetworkOperators.NetworkOperatorTetheringManager,Windows.Networking.NetworkOperators,ContentType=WindowsRuntime]::CreateFromConnectionProfile($profile); $tether.StopTetheringAsync()"
+docker compose -f ../docker-compose.yml up -d
 
 timeout /t 10 /nobreak > NUL
 
